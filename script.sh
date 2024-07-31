42ec80c26f251b2ab45a282c0ebd3d7cf04d695d
Mohamed Elshazly <150818111+MohamedElshazly04@users.noreply.github.com>
2024-07-31 19:50:45 +0300

    frist

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 sudo apt-get install cowsay -y
-cowsay -f dragon "Run for cover, i am a Dragon :p" >> dragon .txt
+cowsay -f dragon "Run for cover, i am a Dragon :p" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
 ls -ltra
